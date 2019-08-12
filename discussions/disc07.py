28214bf4cc2742eae641d3d6033dcca1f2dbda51
Wingchi Mak <mairz1994@outlook.com>
2019-08-12 00:36:38 -0400

    add disc07 and lab07

---
+++ discussions/disc07.py
@@ -0,0 +1,221 @@
+## Q1
+def multiply_lnks(lst_of_lnks):
+    """
+    >>> a = Link(2, Link(3, Link(5)))
+    >>> b = Link(6, Link(4, Link(2)))
+    >>> c = Link(4, Link(1, Link(0, Link(2))))
+    >>> p = multiply_lnks([a, b, c])
+    >>> p.first
+    48
+    >>> p.rest.first
+    12
+    >>> p.rest.rest.rest
+    ()
+    """
+    if not all(lst_of_lnks):
+        return Link.empty
+
+    label = 1
+    for link in lst_of_lnks:
+        label *= link.first
+
+    return Link(label, multiply_lnks([link.rest for link in lst_of_lnks]))
+
+## Q2
+def remove_duplicates(lnk):
+    """
+    >>> lnk = Link(1, Link(1, Link(1, Link(1, Link(5)))))
+    >>> remove_duplicates(lnk)
+    >>> lnk
+    Link(1, Link(5))
+    """
+    if not lnk or not lnk.rest:
+        return
+
+    if lnk.rest.first == lnk.first:
+        lnk.rest = lnk.rest.rest
+        remove_duplicates(lnk)
+    else:
+        remove_duplicates(lnk.rest)
+
+## Q3
+def even_weighted(lst):
+    """
+    >>> x = [1, 2, 3, 4, 5, 6]
+    >>> even_weighted(x)
+    [0, 6, 20]
+    """
+    return [element * lst.index(element) for element in lst if lst.index(element) % 2 == 0]
+
+## Q4
+def quicksort_list(lst):
+    """
+    >>> quicksort_list([3, 1, 4])
+    [1, 3, 4]
+    """
+    if not lst:
+        return
+
+    pivot = lst[0]
+    less = [element for element in lst if element < pivot]
+    greater = [element for element in lst if element > pivot]
+    return less + [pivot] + greater
+
+def max_product(lst):
+    """Return the maximum product that can be formed using lst
+    without using any consecutive numbers
+    >>> max_product([10,3,1,9,2]) # 10 * 9
+    90
+    >>> max_product([5,10,5,10,5]) # 5 * 5 * 5
+    125
+    >>> max_product([])
+    1
+    """
+    if not lst:
+        return 1
+    elif len(lst) <= 2:
+        return max(lst)
+    elif len(lst) == 3:
+        return max(lst[0]*lst[2], lst[1])
+    return max(lst[0]*max_product(lst[2:]), lst[1]*max_product(lst[3:]))
+
+def redundant_map(t, f):
+    """
+    >>> double = lambda x: x*2
+    >>> tree = Tree(1, [Tree(1), Tree(2, [Tree(1, [Tree(1)])])])
+    >>> redundant_map(tree, double)
+    >>> tree
+    Tree(2, [Tree(4), Tree(8, [Tree(16, [Tree(256)])])])
+    """
+    t.label = f(t.label)
+    new_f = lambda x: f(f(x))
+    for b in t.branches:
+        redundant_map(b, new_f)
+
+# Linked List Class
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.second
+    3
+    >>> s.first = 5
+    >>> s.second = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    @property
+    def second(self):
+        return self.rest.first
+
+    @second.setter
+    def second(self, value):
+        self.rest.first = value
+
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+# Tree Class
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def map(self, fn):
+        """
+        Apply a function `fn` to each node in the tree and mutate the tree.
+
+        >>> t1 = Tree(1)
+        >>> t1.map(lambda x: x + 2)
+        >>> t1.map(lambda x : x * 4)
+        >>> t1.label
+        12
+        >>> t2 = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+        >>> t2.map(lambda x: x * x)
+        >>> t2
+        Tree(9, [Tree(4, [Tree(25)]), Tree(16)])
+        """
+        self.label = fn(self.label)
+        for b in self.branches:
+            b.map(fn)
+
+    def __contains__(self, e):
+        """
+        Determine whether an element exists in the tree.
+
+        >>> t1 = Tree(1)
+        >>> 1 in t1
+        True
+        >>> 8 in t1
+        False
+        >>> t2 = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+        >>> 6 in t2
+        False
+        >>> 5 in t2
+        True
+        """
+        if self.label == e:
+            return True
+        for b in self.branches:
+            if e in b:
+                return True
+        return False
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
